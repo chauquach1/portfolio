f34223071087b8375911cf095cf56432d4273a3b
chauquach1 <chau268@gmail.com>
2024-03-24 18:10:27 -0700

    created accordion component for About Me section

---
+++ app/components/sandbox/aboutMeAccordion/AboutMeAccordion.jsx
@@ -0,0 +1,12 @@
+"use client";
+
+export default function AboutMeAccordion() {
+  return (
+    <div className="flex flex-col justify-between flex-grow w-full bg-white/5">
+      <h1 className="text-6xl text-white/30 justify-self-end"></h1>
+      <br />
+      <p className="text-4xl text-white/30 justify-self-end mt-auto">
+      </p>
+    </div>
+  );
+}
