7edcf7d2141fc013b7ee475bfa544afc07342834
chauquach1 <chau268@gmail.com>
2024-04-05 14:53:12 -0700

    updated import statements

--- app/sections/AboutMeSection.jsx
+++ app/sections/AboutMeSection.jsx
@@ -1,14 +1,14 @@
-import WhoIAm from "../blocks/WhoIAm";
-import WhatIDo from "../blocks/WhatIDo";
+import WhoIAmBlock from "../blocks/WhoIAm";
+import WhatIDoBlock from "../blocks/WhatIDo";
 
 export default function AboutMe() {
   return (
     <section
       className={`relative me-auto  overflow-y-hidden min-h-screen min-w-screen max-w-screen flex flex-col self-center items-center overflow-hidden justify-between `}
     >
-      <WhoIAm />
+      <WhoIAmBlock />
 
-      <WhatIDo />
+      <WhatIDoBlock />
     </section>
   );
 }
